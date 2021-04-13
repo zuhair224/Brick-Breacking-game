f39dd132ad5513a747fef3d5d3927d7755717cf1
zuhair224 <75549570+zuhair224@users.noreply.github.com>
2021-04-13 20:23:13 +0200

    Add files via upload

---
+++ Tile.java
@@ -0,0 +1,31 @@
+package sample;
+
+public class Tile {
+
+    int value ;
+    String color;
+
+    public Tile() {
+    }
+
+    public Tile(int value, String color) {
+        this.value = value;
+        this.color = color;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
